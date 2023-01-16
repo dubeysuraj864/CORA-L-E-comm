5dd6ebe77f1fcf16ce60e111de27e5af1ba385a4
Suraj Dubey <dubeysuraj864@gmail.com>
2023-01-16 13:19:41 +0530

    api integration

--- src/components/HandPickedCard.jsx
+++ src/components/HandPickedCard.jsx
@@ -1,10 +1,11 @@
 import Handpickedwatch from "../images/handpickedwatch.png"
 
-function HandPickedCard() {
+function HandPickedCard(props) {
     return ( <>
     <div className="Handpicked-card relative w-[250px] h-fit m-4 rounded-lg ">
-        <img src={Handpickedwatch} className="rounded-lg w-[250px]" alt="" />
-        <div className="absolute bottom-2 w-1/2 text-black left-2 bg-blur backdrop-blur-md">Wrist Watches</div>
+        <img src={props.image} className="rounded-lg w-[250px] h-[250px] bg-white object-contain" alt="" />
+        <div className="absolute bottom-2 w-1/2 text-black left-2 bg-blur backdrop-blur-md text-center rounded-xl">{
+props.category}</div>
     </div>
     </> );
 }
--- src/components/HandpickedCollections.jsx
+++ src/components/HandpickedCollections.jsx
@@ -1,27 +1,30 @@
+import { useContext } from "react";
 import HandPickedCard from "./HandPickedCard";
+import { AppProvider } from "../App";
 
 function HandpickedCollections() {
+  const {item} = useContext(AppProvider);
   return (
     <>
       <div className="new-arrival my-10 py-10 md:p-5 bg-[#1B4B66] text-white">
         <div className="ml-6 text-3xl font-bold">Handpicked Collections </div>
         <div className="flex overflow-auto scrollbar-hide py-5">
-          <div>
-            <HandPickedCard />
-          </div>
-          <div>
-            <HandPickedCard />
-          </div>
-          <div>
-            <HandPickedCard />
-          </div>
-
-          <div>
-            <HandPickedCard />
-          </div>
-          <div>
-            <HandPickedCard />
-          </div>
+          {
+            item.map((x, key)=> {
+              return(
+                <div>
+                <HandPickedCard 
+                   key={key}
+                   image={x.image}
+                   title={x.title}
+                   price={x.price}
+                   rating={x.rating}
+                   category={x.category}/>
+              </div>
+              )
+           
+            })
+          }
         </div>
       </div>
     </>
